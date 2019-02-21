c6646ddd2ad3327802fff5fce5cbeb6a91215826
Kip Streithorst <kstreith@yahoo.com>
2019-02-21 09:29:35 -0500

    Add projects

---
+++ DiveIntoNetCoreConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DiveIntoNetCoreConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var val = r.Next(100);
+            var list = new List<int> { 5, 10, 22 };
+            string.Join(", ", list);
+            var mappedValues = list.Select(x => x * x);
+            var mappedResult = mappedValues.ToList();
+        }
+    }
+}
---
+++ DiveIntoNetCoreWebApp/Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DiveIntoNetCoreWebApp.Controllers
+{
+    public class Person
+    {
+        [Required]
+        [StringLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] Person person)
+        {
+            var isValid = this.ModelState.IsValid;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
